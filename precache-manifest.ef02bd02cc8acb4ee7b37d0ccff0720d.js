313b45b713280edc0e120a848aba9d14171ff873
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2019-04-25 08:19:37 -0400

    Updates

---
+++ precache-manifest.ef02bd02cc8acb4ee7b37d0ccff0720d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f2cb2fd2425cdb6103df867fb6189a",
+    "url": "/React-Clicky_Game/index.html"
+  },
+  {
+    "revision": "a4f27c43a5135cce9323",
+    "url": "/React-Clicky_Game/static/css/main.57d61e53.chunk.css"
+  },
+  {
+    "revision": "85c9e7f0ba8cec633cbe",
+    "url": "/React-Clicky_Game/static/js/2.578ce490.chunk.js"
+  },
+  {
+    "revision": "a4f27c43a5135cce9323",
+    "url": "/React-Clicky_Game/static/js/main.b1f57355.chunk.js"
+  },
+  {
+    "revision": "6cce07a2208ea4a0b147",
+    "url": "/React-Clicky_Game/static/js/runtime~main.66f9b02b.js"
+  }
+]);
